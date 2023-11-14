90843afe5f5bf582ff9dbc3b458a15595eb6c265
Duke <dukemorise33@gmail.com>
2023-11-14 11:37:20 -0800

    fixed post bug that was causing post in dark mode to be white causing the posts to be unreadable since they were white as well

--- src/components/UserPost.js
+++ src/components/UserPost.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { Card } from "semantic-ui-react";
+import { useOutletContext } from "react-router-dom";
 
 function UserPost({ flatpost, setLiked, liked, onHideShow }) {
 
     const { timestamp, author, post, likes, id, isHidden } = flatpost;
     const [likeCount, setLikeCount] = useState(likes);
+
+    const [login, logout, userPassCheckingAlgo, userDataBase, setUserDataBase, onHideShowPost, isDark, switchMode, loggedInUsersPostsList] = useOutletContext();
 
     function onLike() {
         if (!liked) {
@@ -18,26 +21,34 @@ function UserPost({ flatpost, setLiked, liked, onHideShow }) {
     }
 
     return (
-        <Card style={{
-            'overflow': 'hidden',
-            'width': '65%'
-        }}>
-            <Card.Content>
-                {!isHidden ?
-                    <div>
-                        <Card.Header style={{
-                            'font-weight': 'bold',
-                            'font-size': '16px',
-                            'margin-bottom': '5px'
+        <>
+            {!isHidden ?
+                <Card style={!isDark ? {
+                    'overflow': 'hidden',
+                    'width': '65%',
+                } : 
+                {
+                    'overflow': 'hidden',
+                    'width': '65%',
+                    'backgroundColor': 'blue'
+                }}>
+                    <Card.Content>
+                        <div>
+                            <Card.Header style={{
+                                'font-weight': 'bold',
+                                'font-size': '16px',
+                                'margin-bottom': '5px'
                             }}>{author}</Card.Header>
-                        <Card.Description style={{'margin-bottom': '5px'}}>{post}</Card.Description>
-                        <p onClick={() => onLike()} style={{'margin-bottom': '5px'}}>LIKES: {likeCount}</p>
-                        <p style={{'margin-bottom': '5px'}}>{timestamp}</p>
-                    </div>
-                    : null}
-                <button onClick={() => onHideShow(id)}>HIDE?</button>
-            </Card.Content>
-        </Card>
+                            <Card.Description style={{ 'margin-bottom': '5px' }}>{post}</Card.Description>
+                            <p onClick={() => onLike()} style={{ 'margin-bottom': '5px' }}>LIKES: {likeCount}</p>
+                            <p style={{ 'margin-bottom': '5px' }}>{timestamp}</p>
+                        </div>
+                        <button onClick={() => onHideShow(id)}>HIDE?</button>
+                    </Card.Content>
+                </Card >
+                : null
+            }
+        </>
     )
 }
 
