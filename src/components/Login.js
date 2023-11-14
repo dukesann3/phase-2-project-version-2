ff7a4f218939e2914c328afac3fb34c98bcdf981
Duke <dukemorise33@gmail.com>
2023-11-14 12:31:27 -0800

    cleaned up login form using ui semantics

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,12 +5,13 @@ function Login() {
 
     const [login, logout, userPassCheckingAlgo, userDataBase, setUserDataBase, onHideShowPost] = useOutletContext();
     const localLogInStatus = localStorage.getItem('isLoggedIn');
-    const localId = JSON.parse(localStorage.getItem('id')-1);
+    const localId = JSON.parse(localStorage.getItem('id') - 1);
 
     const [form, setForm] = useState({
         username: '',
         password: '',
     });
+    const [loginError, setLoginError] = useState(false);
 
     function handleChange(e) {
         if (localLogInStatus) {
@@ -49,32 +50,60 @@ function Login() {
         }
         else {
             const loginForm = document.querySelector('#loginForm');
-            const alertParagraph = document.createElement('p');
-            alertParagraph.textContent = 'Incorrect Username or Password';
-            loginForm.appendChild(alertParagraph);
+            loginForm.classList.add('error');
+            setLoginError(true);
             setTimeout(() => {
-                loginForm.removeChild(alertParagraph);
+                setLoginError(false);
+                loginForm.classList.remove('error');
             }, 3000);
         }
     }
 
-return (
-    <>
-        <form id='loginForm' onSubmit={(e) => onSubmital(e)}>
-            <input id='username' type='text' name='username' placeholder='username'
-                onChange={(e) => handleChange(e)}
-                value={localLogInStatus ? localStorage.getItem('username') : form.username}
-                style={localLogInStatus ? { 'backgroundColor': 'gray' } : null}
-            />
-            <input id='password' type='password' name='password' placeholder='password'
-                onChange={(e) => handleChange(e)}
-                value={localLogInStatus ? localStorage.getItem('password') : form.password}
-                style={localLogInStatus ? { 'backgroundColor': 'gray' } : null}
-            />
-            <input type='submit' value='SUBMIT' />
+    return (
+        <form id='loginForm' onSubmit={(e) => onSubmital(e)} className="ui small form" style={{
+            'width': '50%',
+        }}>
+            <div className="two fields" style={{
+                'display': 'flex',
+                'flexDirection': 'row',
+                'marginTop': '3%',
+                'marginLeft': '3%'
+            }}>
+                <div className="field">
+                    <label>First Name</label>
+                    <input id='username' type='text' name='username' placeholder='username'
+                        onChange={(e) => handleChange(e)}
+                        value={localLogInStatus ? localStorage.getItem('username') : form.username}
+                        style={localLogInStatus ? { 'backgroundColor': 'gray' } : null}
+                    />
+                </div>
+                <div className="field">
+                    <label>Last Name</label>
+                    <input id='password' type='password' name='password' placeholder='password'
+                        onChange={(e) => handleChange(e)}
+                        value={localLogInStatus ? localStorage.getItem('password') : form.password}
+                        style={localLogInStatus ? { 'backgroundColor': 'gray' } : null}
+                    />
+                </div>
+                <input type='submit' value='SUBMIT' className='ui button submit' />
+            </div>
+            {loginError ?
+                <div className="ui error message" style={{'marginLeft': '3%'}}>
+                    <div className="header">Action Forbidden</div>
+                    <p>Incorrect Username or Password</p>
+                </div> : null}
         </form>
-    </>
-);
+    );
 }
 
 export default Login;
+
+/*
+
+    <div class="field">
+      <label>Last Name</label>
+      <input placeholder="Last Name" type="text">
+    </div>
+  </div>
+
+*/
