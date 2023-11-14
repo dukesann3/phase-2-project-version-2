30834ff092bbae701a470845d9e0171634703916
Duke <dukemorise33@gmail.com>
2023-11-14 13:37:49 -0800

    homepage updated to have CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,19 @@
 
 function Home() {
     return (
-        <div>
-            <h1>This is Homepage</h1>
+        <div style={{
+            'display': 'flex',
+            'flexDirection': 'column',
+            'alignItems': 'center',
+            'justifyContent': 'center'
+        }}>
+            <h1 style={{'marginTop': '5%'}}>WELCOME TO FLATSOCIAL</h1>
+            <img src='./images/hashtag.jpg' alt='hashtag-image' style={{
+                'width': '50%',
+                'height': 'auto',
+                'marginTop': '3%',
+                'marginBottom': '3%'
+            }}/>
         </div>
     );
 }
